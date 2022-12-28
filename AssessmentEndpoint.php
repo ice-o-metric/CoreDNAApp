174ccb8552087944c4a2fb28bda28d50fab74445
ice-o-metric <receive.attachments@gmail.com>
2022-12-28 15:12:52 +1100

    Update AssessmentEndpoint.php
    Send as JSON body

--- AssessmentEndpoint.php
+++ AssessmentEndpoint.php
@@ -1,30 +1,32 @@
 <?php
-	class AssessmentEndpoint{
+    class AssessmentEndpoint{
         //based on https://stackoverflow.com/questions/8596311/how-to-make-a-post-request-without-curl
-		
-		function SendAndOutput($name, $email, $git) {
-			require_once('RequestMaker.php');
-			
-			$connector = new RequestMaker();
-			
-			echo '<p>Instantiated</p>';
-			
-			$token = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'OPTIONS');
-			
-			echo '<p>TOKEN:' . json_encode($token) . '</p>';
-			
-			$header = 'Authorization: Bearer ' . $token;
-			
-			$data = [
-				'name'	=> $name,
-				'email'	=> $email,
-				'url'	=> $git
-			];
-			
-			$response = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'POST', $data);
-			
-			echo '<p>RESPONCE:' . json_encode($response) . '</p>';
-			
-			echo '<p>Done</p>';
-		}
+        
+        function SendAndOutput($name, $email, $git) {
+            require_once('RequestMaker.php');
+            
+            $connector = new RequestMaker();
+            
+            echo '<p>Instantiated</p>';
+            
+            $token = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'OPTIONS');
+            
+            echo '<p>TOKEN:' . json_encode($token) . '</p>';
+            
+            $header  = 'Authorization: Bearer ' . $token . "\r\n";
+            $header .= 'Content-type: application/json';
+            
+            $data = [
+                'name'   => $name,
+                'email'  => $email,
+                'url'    => $git
+            ];
+            
+            $response = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'POST', $data, $header);
+            
+            echo '<p>RESPONCE:' . json_encode($response) . '</p>';
+            
+            echo '<p>Done</p>';
+        }
+    }
 ?>
