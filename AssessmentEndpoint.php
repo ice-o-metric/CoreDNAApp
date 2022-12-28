e07a00678d9661d23e82f46ef072158cd925823a
ice-o-metric <receive.attachments@gmail.com>
2022-12-28 14:20:49 +1100

    Update AssessmentEndpoint.php
    Convert to class

--- AssessmentEndpoint.php
+++ AssessmentEndpoint.php
@@ -1,21 +1,30 @@
 <?php
-	require_once('RequestMaker.php');
-	
-	$connector = new RequestMaker();
-	
-	echo '<p>Instantiated</p>';
-	
-	$token = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'OPTIONS');
-	
-	echo '<p>TOKEN:' . json_encode($token) . '</p>';
-	
-	$header = 'Authorization: Bearer ' . $token;
-	
-	$data = [
-		'name'	=> 'Daniel Vida',
-		'email'	=> 'receive.attachments@gmail.com',
-		'url'	=> 'https://github.com/ice-o-metric/CoreDNAApp'
-	];
-	
-	echo '<p>Done</p>';
+	class AssessmentEndpoint{
+        //based on https://stackoverflow.com/questions/8596311/how-to-make-a-post-request-without-curl
+		
+		function SendAndOutput($name, $email, $git) {
+			require_once('RequestMaker.php');
+			
+			$connector = new RequestMaker();
+			
+			echo '<p>Instantiated</p>';
+			
+			$token = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'OPTIONS');
+			
+			echo '<p>TOKEN:' . json_encode($token) . '</p>';
+			
+			$header = 'Authorization: Bearer ' . $token;
+			
+			$data = [
+				'name'	=> $name,
+				'email'	=> $email,
+				'url'	=> $git
+			];
+			
+			$response = $connector->Request('https://corednacom.corewebdna.com/assessment-endpoint.php', 'POST', $data);
+			
+			echo '<p>RESPONCE:' . json_encode($response) . '</p>';
+			
+			echo '<p>Done</p>';
+		}
 ?>
